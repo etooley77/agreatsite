98cb371feeb43c8a55303878c64b14b6da898f4b
etooley77 <133033128+etooley77@users.noreply.github.com>
2023-06-30 11:55:35 -0400

    Added files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+function clickbutton() {
+    alert("You clicked the button. How dare you...")
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUjcmljayBhc3RsZXkgbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D";
+
+}
